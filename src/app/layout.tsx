98e9c0d2458d81bfda0383c22278c18cc9f7363e
Mohamed Hassan <me@mohassan.dev>
2025-09-19 13:06:55 +0300

    add vercel's analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,48 +1,58 @@
 import type { Metadata } from "next";
-import { AppShell, AppShellHeader, AppShellMain, ColorSchemeScript, MantineProvider } from "@mantine/core";
+import {
+  AppShell,
+  AppShellHeader,
+  AppShellMain,
+  ColorSchemeScript,
+  MantineProvider,
+} from "@mantine/core";
 import theme from "./theme";
 import Header from "@/components/Header";
 import { epilogue } from "./fonts";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 import "./globals.css";
 import "@mantine/core/styles.css";
 
 export const metadata: Metadata = {
-	title: "Mohamed Hassan",
-	description: "Software developer",
+  title: "Mohamed Hassan",
+  description: "Software developer",
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<head>
-				<ColorSchemeScript defaultColorScheme="light" />
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-				<link rel="icon" href="/favicon.svg" />
-			</head>
+  return (
+    <html lang="en">
+      <head>
+        <ColorSchemeScript defaultColorScheme="light" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.svg" />
+        <SpeedInsights />
+        <Analytics />
+      </head>
 
-			<body>
-				<MantineProvider theme={theme} defaultColorScheme="light">
-					<AppShell
-						padding="md"
-						h="100dvh"
-						header={{
-							height: 80,
-						}}
-						className={epilogue.className}
-					>
-						<AppShellHeader withBorder={false}>
-							<Header />
-						</AppShellHeader>
+      <body>
+        <MantineProvider theme={theme} defaultColorScheme="light">
+          <AppShell
+            padding="md"
+            h="100dvh"
+            header={{
+              height: 80,
+            }}
+            className={epilogue.className}
+          >
+            <AppShellHeader withBorder={false}>
+              <Header />
+            </AppShellHeader>
 
-						<AppShellMain>{children}</AppShellMain>
-					</AppShell>
-				</MantineProvider>
-			</body>
-		</html>
-	);
+            <AppShellMain>{children}</AppShellMain>
+          </AppShell>
+        </MantineProvider>
+      </body>
+    </html>
+  );
 }
